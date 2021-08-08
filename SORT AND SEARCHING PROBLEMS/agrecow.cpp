d266c712837ada2cad161ee15e6ad12ee268aa43
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-08-09 00:48:09 +0530

    Update agrecow.cpp

---
+++ SORT AND SEARCHING PROBLEMS/agrecow.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool check(vector<int> a,int n,int )
+
+int result(vector<int> a,int l,int r,int cow)
+{
+    sort(a.begin(), a.end());
+    m
+
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int len,cow;
+        cin >> len>>cow;
+        vector<int> stall;
+        for(int i=0;i<len;i++){
+            int x;
+            cin >> x;
+            stall.push_back(x);
+        }
+    }
+}
