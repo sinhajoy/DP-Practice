af0100bfa4360f93dc0f5396fed5011796cdfebe
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-08-25 01:11:18 +0530

    t

--- LINKED LIST/insertion.cpp
+++ LINKED LIST/insertion.cpp
@@ -1,25 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-struct Node{
-    int data;
-    struct Node* next;
+class Node{
+    public:
+        int data;
+        No
 };
-
-struct Node* head= NULL;
-
-void insert_head(int x){
-    struct Node* new_node = new Node();
-    new_node ->data = x;
-    new_node ->next =NULL;
-    if(head!=NULL){
-        new_node ->next =NULL;
-    }
-    head = new_node;
-}
-
-
-int main()
-{
-    insert_head(1);
-}
---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int tail=0;
+    _tail=1;
+    cout<<tail;
+}
