9d7c9b689a34967ddc0e52f7b21174d93fab7183
JOY SINHA <51963863+sinhajoy@users.noreply.github.com>
2021-08-31 11:29:16 +0530

    cloin

---
+++ STACK/cmake-build-debug/CMakeFiles/STACK.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/STACK.dir/main.cpp.obj"
+  "CMakeFiles/STACK.dir/nearest-grater-to-right.cpp.obj"
+  "STACK.exe"
+  "STACK.exe.manifest"
+  "STACK.pdb"
+  "libSTACK.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/STACK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
